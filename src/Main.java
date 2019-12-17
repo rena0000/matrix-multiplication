175a642f161ac5d5e1bbb827c2aaa66e60c8648d
Serena He <SHe@verathon.com>
2019-12-16 17:52:14 -0800

    Dev #3 Add function for running matrix multiplication, broken

--- src/Main.java
+++ src/Main.java
@@ -9,50 +9,6 @@ import java.util.Random;
 
 public class Main {
 
-    static int[][] MATRIX_3x2 = new int[][] {
-            {1, 2, 3},
-            {4, 5, 6}
-    };
-    static int[][] MATRIX_2x3 = new int[][]{
-            {7, 8},
-            {9, 10},
-            {11, 12}
-    };
-
-    static int[][] singleThreadMultiplyMatrix(int[][] matrix1, int[][] matrix2) {
-        /**
-         * Finds the dot product of two matrices using a single thread.
-         * @param int[][] Left-side matrix for dot product
-         * @param int[][] Right-side matrix for dot product
-         * @return int[][] Dot product of two given matrices.
-         */
-        int newRows = matrix1.length;
-        int newCols = matrix2[0].length;
-        int[][] solution = new int[newRows][newCols];
-
-        // Check that matrices can be multiplied
-        try {
-            if (matrix2.length != matrix1[0].length) {
-                throw new ArrayIndexOutOfBoundsException("Error");
-            }
-            else {
-                for (int row=0; row < newRows; row++) {
-                    for (int col=0; col < newCols; col++) {
-                        solution[row][col] = 0;
-                        for (int i=0; i < matrix2.length; i++) {
-                            solution[row][col] += matrix1[row][i] * matrix2[i][col];
-                        }
-                    }
-                }
-            }
-        }
-        catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("Cannot find dot product of matrices: " + e);
-        }
-
-        return solution;
-    }
-
     static void printMatrix(int[][] matrix) {
         /**
          * Prints a given matrix to console.
@@ -87,6 +43,60 @@ public class Main {
         return newMatrix;
     }
 
+    static long runMatrixMultiplication(int[][] matrix1, int[][] matrix2, int numThreads) {
+        long startTime = System.nanoTime();
+        MultiplierThread[] threads;
+        int threadSectionLength;
+        int matrixRows = matrix1.length;
+
+        // Make sure matrix1 rows can be divided into even sections
+        try {
+            if (matrixRows % numThreads != 0 ) {
+                throw new ArrayIndexOutOfBoundsException("Matrix rows cannot be cleanly divided by the number of threads");
+            }
+            else {
+                threadSectionLength = matrixRows / numThreads;
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Exception caught: " + e);
+        }
+
+        int[][] quad_matrix1_1 = Arrays.copyOfRange(matrix1, 0, quadSectionLength);
+        int[][] quad_matrix1_2 = Arrays.copyOfRange(matrix1, quadSectionLength, 2*quadSectionLength);
+        int[][] quad_matrix1_3 = Arrays.copyOfRange(matrix1, 2*quadSectionLength, 3*quadSectionLength);
+        int[][] quad_matrix1_4 = Arrays.copyOfRange(matrix1, 3*quadSectionLength, matrix1Rows);
+
+        threads = new MultiplierThread[numThreads];
+
+        for (int i=0; i < numThreads; i++) {
+            int[][] matrixSection = Arrays.copyOfRange(matrix1, i*threadSectionLength, (i+1)*threadSectionLength);
+            MultiplierThread newThread = new MultiplierThread(matrixSection, matrix2);
+            threads[i] = newThread;
+            newThread.start();
+        }
+
+        MultiplierThread thread21 = new MultiplierThread(double_matrix1_1, matrix2);
+        MultiplierThread thread22 = new MultiplierThread(double_matrix1_2, matrix2);
+        thread21.start();
+        thread22.start();
+        try {
+            thread21.join();
+            thread22.join();
+
+            doubleThreadResult = new int[matrix1Rows][];
+            System.arraycopy(thread21.resultMatrix, 0, doubleThreadResult, 0, thread21.resultMatrix.length);
+            System.arraycopy(thread22.resultMatrix, 0, doubleThreadResult, thread21.resultMatrix.length, thread22.resultMatrix.length);
+            //printMatrix(doubleThreadResult);
+        }
+        catch (Exception e) {
+            System.out.println("Exception caught: " + e);
+        }
+
+        long endTime = System.nanoTime();
+        return (endTime - startTime)/1000000;
+    }
+
     public static void main(String[] args) {
         // Times
         long startTime;
@@ -100,8 +110,8 @@ public class Main {
         int[][] doubleThreadResult;
         int[][] quadrupleThreadResult;
         // Generate matrices (1000x1000)
-        int [][] matrix1 = generateRandomMatrix(24, 24, -20, 20);
-        int [][] matrix2 = generateRandomMatrix(24, 24, -20, 20);
+        int [][] matrix1 = generateRandomMatrix(4, 4, -20, 20);
+        int [][] matrix2 = generateRandomMatrix(4, 4, -20, 20);
         int matrix1Rows = matrix1.length;
 
         // -----Single thread-----
@@ -113,7 +123,7 @@ public class Main {
         try {
             thread11.join();
             singleThreadResult = thread11.resultMatrix;
-            printMatrix(singleThreadResult);
+            //printMatrix(singleThreadResult);
         }
         catch (Exception e) {
             System.out.println("Exception caught: " + e);
@@ -122,7 +132,6 @@ public class Main {
         endTime = System.nanoTime();
         duration = (endTime - startTime)/1000000;
         singleTime = duration;
-        System.out.println("Time: " + duration + " ms\n");
 
         // -----Two threads-----
         startTime = System.nanoTime();
@@ -141,7 +150,7 @@ public class Main {
             doubleThreadResult = new int[matrix1Rows][];
             System.arraycopy(thread21.resultMatrix, 0, doubleThreadResult, 0, thread21.resultMatrix.length);
             System.arraycopy(thread22.resultMatrix, 0, doubleThreadResult, thread21.resultMatrix.length, thread22.resultMatrix.length);
-            printMatrix(doubleThreadResult);
+            //printMatrix(doubleThreadResult);
         }
         catch (Exception e) {
             System.out.println("Exception caught: " + e);
@@ -150,7 +159,6 @@ public class Main {
         endTime = System.nanoTime();
         duration = (endTime - startTime)/1000000;
         doubleTime = duration;
-        System.out.println("Time: " + duration + " ms");
 
         // -----Four threads-----
         startTime = System.nanoTime();
@@ -184,7 +192,7 @@ public class Main {
             System.arraycopy(thread43.resultMatrix, 0, quadrupleThreadResult, 2*quadSectionLength, quadSectionLength);
             System.arraycopy(thread44.resultMatrix, 0, quadrupleThreadResult, 3*quadSectionLength, quadSectionLength);
 
-            printMatrix(quadrupleThreadResult);
+            //printMatrix(quadrupleThreadResult);
         }
         catch (Exception e) {
             System.out.println("Exception caught: " + e);
@@ -193,7 +201,6 @@ public class Main {
         endTime = System.nanoTime();
         duration = (endTime - startTime)/1000000;
         quadrupleTime = duration;
-        System.out.println("Time: " + duration + " ms");
 
         // -----Print results-----
         System.out.println("=====================RESULTS=====================");
